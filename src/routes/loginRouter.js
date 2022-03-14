bca28b57b7bec4a97011ea24b07e2143d8edc71f
zhangjinzhe <zhangjinzhe@88.com>
2022-03-14 12:17:58 +0800

    添加路由前缀

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -3,6 +3,8 @@ import loginController from '../api/loginController'
 import koaRouter from '@koa/router'
 
 const router = new koaRouter()
+router.prefix('/api')
+
 router.get('/getCaptcha', loginController.getCaptcha)
 router.get('/sendMail', loginController.sendMail)
 router.post('/updatePwd', loginController.updatePwd)
