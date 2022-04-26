e265a289c578a32ac5ae7de784afd7e9bda8a1ab
zhangjinzhe <zhangjinzhe@88.com>
2022-04-26 19:32:55 +0800

    fix:mongo问题

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export const JWT_SECRET = 'sdfjhbgjhbejrgbwurgbndbjhwebfhwhguybgjhebrjnvbd'
-export const DB_URL = 'mongodb://website:wsZhang17.@101.43.153.234:27017/website'
+export const DB_URL = 'mongodb://admin:mgZhang17.@101.43.153.234:27017/website'
